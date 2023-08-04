feb918eacf95e78a1a9b93960a1b11789363c03c
L1senochek <lisenochek96@gmail.com>
2023-08-04 19:05:59 +0300

    feat: add eslint & prettier config

--- eCommerceApplication/src/pages/app/app.test.tsx
+++ eCommerceApplication/src/pages/app/app.test.tsx
@@ -1,5 +1,5 @@
-import  React, { ReactElement } from 'react';
-import { expect, test } from 'vitest'
+import React, { ReactElement } from 'react';
+import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './app';
 
