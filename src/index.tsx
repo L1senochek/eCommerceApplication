a3f40286006b4004885955aba77bbfd3d40e5975
l1senochek <lisenochek96@gmail.com>
2023-08-08 17:22:21 +0300

    feat: add style for body

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/app/app';
-// import './index.css'
+import './index.scss';
 
 const root = document.getElementById('root');
 
