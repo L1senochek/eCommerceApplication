c920028a1f3144cb4be77933e15399ff065333f2
l1senochek <lisenochek96@gmail.com>
2023-08-08 12:34:19 +0300

    feat: create login form

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -1,7 +1,66 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, ChangeEvent, FormEvent } from 'react';
+
+const Username = (): ReactElement => {
+  const [username, setUsername] = useState('');
+
+  const usernameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setUsername(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      className="login-form__username"
+      placeholder="username"
+      id="username"
+      value={username}
+      onChange={usernameChange}
+    />
+  );
+};
+
+const Password = (): ReactElement => {
+  const [password, setPassword] = useState('');
+
+  const passwordChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+    <input
+      type="password"
+      className="login-form__password"
+      placeholder="password"
+      id="password"
+      value={password}
+      onChange={passwordChange}
+    />
+  );
+};
+
+const submit = (event: FormEvent<HTMLFormElement>): void => {
+  event.preventDefault();
+  console.log(event);
+  // отправка данных на сервер для аутентификации
+};
 
 const LoginForm = (): ReactElement => {
-  return <form className="login-form__wrapper"></form>;
+  return (
+    <form className="login-form__wrapper" onSubmit={submit}>
+      <h2 className="login-form__title">Login</h2>
+      <label className="login-form__label" htmlFor="username">
+        Username
+      </label>
+      <Username />
+      <label className="login-form__label">Username</label>
+      <Password />
+      <button className="login-form__submit" type="submit">
+        Login
+      </button>
+    </form>
+  );
 };
 
 export default LoginForm;
