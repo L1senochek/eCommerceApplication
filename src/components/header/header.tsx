2c2076ba03a10125057ac7eee163c2d23944a299
l1senochek <lisenochek96@gmail.com>
2023-08-09 10:21:35 +0300

    feat: add search input & scss

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,47 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import './header.scss';
 import { Link } from 'react-router-dom';
 
+const Search = (): ReactElement => {
+  const [search, setSearch] = useState('');
+
+  const searchChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setSearch(event.target.value);
+    console.log(event.target.value);
+  };
+  return (
+    <div className="search input">
+      <button className="search__loupe"></button>
+      <input
+        type="search"
+        className="search__input"
+        placeholder="Search..."
+        id="search"
+        value={search}
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
 const Header = (): ReactElement => {
   return (
-    <header>
-      <nav>
-        <div className="logo">
-          <Link to="/main">
-            <div className="logo__img"></div>
-            <div className="logo__title">fdfdsf</div>
-          </Link>
-        </div>
-        Header
-        <ul>
-          <li>Menu</li>
-          <li>About</li>
-        </ul>
-        <input type="search" />
-        <button className="login btn">
-          <Link to="/loginForm">login</Link>
-        </button>
+    <header className="header">
+      <nav className="nav">
+        <Link className="logo link" to="/main">
+          <span className="logo__img"></span>
+          <span className="logo__title">logo</span>
+        </Link>
+        <Link className="menu link" to="/menu">
+          Menu
+        </Link>
+        <Link className="about link" to="/about">
+          about
+        </Link>
+        <Search />
+        <Link className="login link" to="/loginForm">
+          login
+        </Link>
       </nav>
     </header>
   );
