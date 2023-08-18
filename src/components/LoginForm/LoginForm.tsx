42bd8520fceb73fd7d40c40e80be6678e73284d0
l1senochek <lisenochek96@gmail.com>
2023-08-18 11:13:09 +0300

    feat: add LoginForm & scss

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,7 +4,7 @@ import UserEmailInput from '../UserEmailInput/UserEmailInput';
 import LinkTo from '../LinkTo/LinkTo';
 import ForgotPasswordLink from '../ForgotPasswordLink/ForgotPasswordLink';
 import Button from '../Button/Button';
-import LinkToWithText from '../LinkToWithTextInWrapper/LinkToWithTextInWrapper';
+import LinkToWithTextInWrapper from '../LinkToWithTextInWrapper/LinkToWithTextInWrapper';
 import PasswordInput from '../PasswordInput/PasswordInput';
 import AuthenticationForm from '../AuthenticationForm/AuthenticationForm';
 import './LoginForm.scss';
@@ -26,9 +26,9 @@ const LoginForm = (): JSX.Element => {
       <PasswordInput placeholder="Password" showError={showError} />
       <ForgotPasswordLink />
       <Button type="submit" text="Sign in" className="authentication-form__submit btn" />
-      <LinkToWithText text="Dont`n have an account? ">
+      <LinkToWithTextInWrapper text="Dont`n have an account? ">
         <LinkTo to={'/registrationForm'} text={'Register here'} />
-      </LinkToWithText>
+      </LinkToWithTextInWrapper>
     </AuthenticationForm>
   );
 };
