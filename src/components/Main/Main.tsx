f3a49f9669651b67b7aeb62d9b96292d5a1af592
l1senochek <lisenochek96@gmail.com>
2023-08-30 10:36:15 +0300

    feat: add to main page btn & scss

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './main.scss';
+import { Link } from 'react-router-dom';
+import { MENU_PAGE } from '../../utils/constants/paths';
 // import TestApi from './testApi';
 
 const Main = (): JSX.Element => {
@@ -9,6 +11,11 @@ const Main = (): JSX.Element => {
         <div className="container-slider__logo">
           <div className="container-slider__logo_img"></div>
         </div>
+        <button className="container-slider__btn btn">
+          <Link to={MENU_PAGE} className="container-slider__link link">
+            Order now
+          </Link>
+        </button>
         <div className="container-slider__first-img"></div>
       </div>
     </main>
