9ee76b2d6e45a9d20d637ce5af1aedf8cfbfc5ae
l1senochek <lisenochek96@gmail.com>
2023-08-31 14:36:42 +0300

    feat: add ProductType component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,10 +1,12 @@
 import Categories from '../Categories/Categories';
+import ProductType from '../ProductType/ProductType';
 import './menu.scss';
 
 const Menu = (): JSX.Element => {
   return (
     <div className="menu">
       <Categories />
+      <ProductType />
       {/* <Products /> */}
     </div>
   );
---
+++ src/components/ProductType/ProductType.tsx
@@ -0,0 +1,11 @@
+import './productType.scss';
+
+const ProductType = (): JSX.Element => {
+  return (
+    <>
+      <div>type</div>
+    </>
+  );
+};
+
+export default ProductType;
