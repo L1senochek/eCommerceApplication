202b9854eea4f342bfeb38b840edbeaba0d92f42
l1senochek <lisenochek96@gmail.com>
2023-08-31 17:38:40 +0300

    feat: add on page products

--- src/api/getAllCategories.ts
+++ src/api/getAllCategories.ts
@@ -5,7 +5,6 @@ export const getAllCategoriesApi = async (): Promise<false | Category[]> => {
   try {
     const response = await getApiRoot().withProjectKey({ projectKey }).categories().get().execute();
 
-    console.log(response);
     if (response.statusCode === 200) {
       return response.body.results;
     } else {
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from 'react';
 import { getAllProducts } from '../../api/getAllProducts';
 import './products.scss';
+import { Product } from '@commercetools/platform-sdk';
 
 const Products = (): JSX.Element => {
-  getAllProducts();
+  const [productsItems, setProductsItems] = useState<Product[]>([]);
+
+  useEffect(() => {
+    (async (): Promise<void> => {
+      const responseArr = await getAllProducts();
+      if (responseArr) {
+        console.log(responseArr);
+        setProductsItems(responseArr);
+      }
+    })();
+  }, []);
+
+  const createProductsItems = (): JSX.Element[] => {
+    return productsItems.map((item) => {
+      return (
+        <div key={item.id} className="product__item">
+          <img
+            className="product__img"
+            src={
+              item.masterData.current.masterVariant.images &&
+              item.masterData.current.masterVariant.images[0].url
+            }
+          />
+          <h2 className="product__title">{item.masterData.current.name['en-US']}</h2>
+          <h3 className="product__description">
+            {item.masterData.current.description && item.masterData.current.description['en-US']}
+          </h3>
+          <h2 className="product__price">
+            {item.masterData.current.masterVariant.prices &&
+              item.masterData.current.masterVariant.prices[0].value.centAmount / 100}
+            $
+          </h2>
+          {/* <h2>
+            {item.masterData.current.masterVariant.prices &&
+              item.masterData.current.masterVariant.prices[1].value.centAmount / 100}
+            €
+          </h2> */}
+          <button className="product__add btn">add</button>
+        </div>
+      );
+    });
+  };
+
   return (
     <>
-      <div>Products</div>
+      <div className="products">{createProductsItems()}</div>
     </>
   );
 };
