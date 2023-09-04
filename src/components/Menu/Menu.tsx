53c70046e898e52aa0898b7ac0e73c13ace56d7e
l1senochek <lisenochek96@gmail.com>
2023-09-04 17:27:20 +0300

    feat: add getAllProductsByProductTypeId

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,11 @@
+import { getAllProductsByProductTypeId } from '../../api/getAllProductsByProductTypeId';
 import Categories from '../Categories/Categories';
 import ProductType from '../ProductType/ProductType';
 import Products from '../Products/Products';
 import './menu.scss';
 
 const Menu = (): JSX.Element => {
+  getAllProductsByProductTypeId();
   return (
     <div className="menu-bar">
       <div className="left-side-bar">
