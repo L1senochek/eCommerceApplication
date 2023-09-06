dd9385e3c0f7d5abd9b3d6746cac0ccd7f6102be
l1senochek <lisenochek96@gmail.com>
2023-09-06 14:20:49 +0300

    feat: add search res in state

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,22 @@
 import { ChangeEvent, ReactElement, useState } from 'react';
 import getProductsSearch from '../../api/getProductsSearch';
+import { ProductProjection } from '@commercetools/platform-sdk';
 
 const Search = (): ReactElement => {
   const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState<ProductProjection[]>([]);
 
   const searchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setSearch(event.target.value);
-    console.log(event.target.value);
+    console.log(event.target.value, search);
   };
 
   const handleSearchProductClick = async (): Promise<void> => {
-    const res = await getProductsSearch('spicy');
-    console.log(res);
+    const res = await getProductsSearch(search);
+    if (res) {
+      setSearchResults(res || []);
+      console.log(search, res, searchResults);
+    }
   };
 
   return (
